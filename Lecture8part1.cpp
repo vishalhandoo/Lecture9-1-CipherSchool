98a60342efbfc501b612239c3f9fdf0ad47ffff3
vishalhandoo <81742803+vishalhandoo@users.noreply.github.com>
2023-06-16 22:59:46 +0530

    Add files via upload

---
+++ Lecture8part1.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int compare(int a,int b){
+    if(a==5){
+        return 1;
+    }
+    return 0;
+}
+
+int main(){
+    // int password;            while loop
+
+    // cout<<"Enter the password: "<<endl;
+    // cin>>password;
+
+    // while(password<99999){
+    //     cout<<"The password does not meet the required condition,Please enter the password again"<<endl;
+    //     cin>>password;
+    // }
+
+    // cout<<"The user has now entered the required password"<<endl;
+
+    cout<<compare(5,6);
+
+    return 0;
+}
